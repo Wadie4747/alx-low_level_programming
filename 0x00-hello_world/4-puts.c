176ac799ed5cc962ce4675744116cd95ae701ac6
Wadie Sabri <wadie.sabri@gmail.com>
2023-06-15 18:03:36 -0700

    first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - main entry
+ * return - 0=success
+ */
 int main(void)
 {
-	if puts("\"Programming is like building a multilingual puzzle");
+	if (puts("\"Programming is like building a multilingual puzzle"));
 	return (0);
 }
