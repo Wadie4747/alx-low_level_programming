0895ce43e02e6caa2d467e04d62b936427557b25
Wadie Sabri <wadie.sabri@gmail.com>
2023-06-21 18:53:01 -0700

    hope time tab done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,32 +9,32 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n < 0 || n > 15)
-		return;
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = j * i;
-			if (j > 0)
+			_putchar('0');
+			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
-				if (k < 10)
+				k = i * j;
+				if (k <= 99)
 					_putchar(' ');
-				if (k < 100)
+				if (k <= 9)
 					_putchar(' ');
+				if (k >= 100)
+				{
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+				}
+				else if (k <= 99 && k >= 10)
+				{
+					_putchar((k / 10) + '0');
+				}
+				_putchar((k % 10) + '0');
 			}
-			if (k < 10)
-				_putchar(' ');
-			else if (k < 100)
-				_putchar(k / 10 + '0');
-			else
-				_putchar(k / 100 + '0');
-			if (k < 100)
-				_putchar(k / 10 % 10 + '0');
-			_putchar(k % 10 + '0');
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
