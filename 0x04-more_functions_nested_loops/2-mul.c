215f6173c99c60051b058f51e5853b1e5743f3fb
Wadie Sabri <wadie.sabri@gmail.com>
2023-06-22 18:48:51 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiply to integers
+ * @a: a - variable
+ * @b: b - variable
+ *
+ * Return: Always 0 (Success).
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
