84803742e25d76ea9e42251d18c97ffd410d2a09
Wadie Sabri <wadie.sabri@gmail.com>
2023-06-22 18:59:31 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers, from 0 to 9,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success).
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
